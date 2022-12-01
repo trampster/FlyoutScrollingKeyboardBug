5d5f3c0874622201a15ca078da0fe96fb1a6a496
Daniel Hughes <daniel.hughes@taitcommunications.com>
2022-12-02 11:49:14 +1300

    Add code which reproduces the bug

---
+++ FlyoutScrollingKeyboardBug/FlyoutScrollingKeyboardBug/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using FlyoutScrollingKeyboardBug.ViewModels;
+
+namespace FlyoutScrollingKeyboardBug.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
